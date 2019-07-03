33704a4dddcc01acb25e883cbf8d32d49b037a59
Turtle P.U <sokolov.p64@gmail.com>
2019-07-03 19:20:15 +0300

    finished join

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import * as Cookie from './js/cookie';
-import $ from './js/util';
-
-document.addEventListener('load', main);
+import { $ } from './js/util';
 
 /**
  * @type {HTMLAnchorElement}
@@ -14,6 +12,11 @@ var a_back;
 var a_join;
 
 /**
+ * @type {HTMLButtonElement}
+ */
+var b_rand;
+
+/**
  * @type {HTMLInputElement}
  */
 var inp_room;
@@ -21,6 +24,7 @@ var inp_room;
 function main() {
     a_back = $('a-back');
     a_join = $('a-join');
+    b_rand = $('b-rand');
     inp_room = $('inp-room');
 
     a_back.href += Cookie.get('room');
@@ -29,9 +33,16 @@ function main() {
         a_join.href += inp_room.value.replace(/\s/g, '');
     });
 
+    b_rand.addEventListener('click', async () => {
+        inp_room.value = await (await fetch('/room_id')).text();
+        a_join.click();
+    });
+
     inp_room.addEventListener('keypress', event => {
         if (event.key == 'Enter') {
             a_join.click();
         }
     });
 }
+
+main();
--- server/src/server.js
+++ server/src/server.js
@@ -6,13 +6,49 @@ import path    from 'path';
 const folder = process.env.DEV_SERVER ? 'dist' : 'build';
 const port   = +process.env.PORT || 3000;
 
-const client = path.join(__dirname, `../../client/${folder}/`);
+const client_dir = path.join(__dirname, `../../client/${folder}/`);
+const pages = new Map(
+    ['game', 'index', 'notfound', 'room'].map(name =>
+        [name, path.join(client_dir, `${name}.html`)]
+    )
+);
 
 const app = express();
 const server = new http.Server(app);
 
+app.use(express.static(client_dir));
+
 app.get('/', (_, res) => {
-    res.sendFile(path.join(client, 'index.html'));
+    res.sendFile(pages.get('index'));
+});
+
+// TODO
+const Games = {
+    can_join: (_, __) => false
+};
+
+app.get('/game/:gameId?/socket/:socketId?', (req, res) => {
+    res.sendFile(pages.get(
+        Games.can_join(req.params.gameId, req.params.socketId)
+        ? 'game' : 'notfound'
+    ));
+});
+
+// TODO
+const Rooms = {
+    join_id: () => 'a',
+    can_join: _ => false
+};
+
+app.get('/room_id', (_, res) => {
+    res.send(Rooms.join_id());
+});
+
+app.get('/room/:roomId?', (req, res) => {
+    res.sendFile(pages.get(
+        Rooms.can_join(req.params.roomId)
+        ? 'room' : 'notfound'
+    ));
 });
 
 server.listen(port, () => {
