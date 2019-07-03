3ecf791676e9d4fab11c775843409be2883110fa
Turtle P.U <sokolov.p64@gmail.com>
2019-07-03 17:30:28 +0300

    + cookies

---
+++ client/src/js/cookie.js
@@ -0,0 +1,47 @@
+/**
+ * Gets a cookie by given name.
+ *
+ * @export
+ * @param {string} name name of the cookie
+ * @returns cookie by given name.
+ */
+export function get(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+/**
+ * Sets new cookie on given name.
+ *
+ * @export
+ * @param {string} name name of the new cookie
+ * @param {string} value
+ * @param {{ expires?: number | Date }} [options={}]
+ */
+export function set(name, value, options = {}) {
+    if (options.expires) {
+        if (typeof options.expires == 'number') {
+            let d = new Date();
+            d.setTime(d.getTime() + options.expires * 1000);
+            options.expires = d;
+        } else if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString();
+        }
+    }
+
+    value = encodeURIComponent(value);
+
+    let updatedCookie = `${name}=${value}`;
+
+    for (let propName in options) {
+        updatedCookie += `;${propName}`;
+        var propValue = options[propName];
+        if (propValue !== true) {
+            updatedCookie += `=${propValue}`;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
